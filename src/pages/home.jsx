4edb272d86a368c915605fad4dcf66a0fa848d97
Anuj Kumar <anujkr8674@example.com>
2025-02-05 11:25:32 +0530

    Fixed dependency issues

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-// import Navbar from "../components/Navbar";
-// import HeroSection from "../components/HeroSection";
-// import Footer from "../components/Footer";
 
-// import Navbar from "../components/navbar";
-// import Footer from "../components/footer";
 import Body from "../components/Body";
 
 const Home = () => {
